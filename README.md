2deea050a15b5fb8d10b6045e0e5d732280ef019
unknown <mohammadahmermalick@gmail.com>
2022-08-14 16:30:23 +0500

    dev: lint staged updated

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 # Clothes Customizer
 
-To run this project 
+To run this project
 run
+
 ### `npm i`
+
 ### `npm start`
+
 in root directory "/"
 
 run
+
 ### `npm i`
+
 ### `npm start`
+
 in server directory "/test_server/"
 
 ## Note
+
 This project runs on local server and also uses "https://unsplash.com/developers" API
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,8 @@ module.exports = {
   // Type check TypeScript files
   '*/.(ts|tsx)': () => 'yarn tsc --noEmit',
 
-  '*.{js,ts,tsx,json,md,prettierrc}': 'prettier --write --config .prettierrc .',
+  '*.{js,ts,tsx,json,md,prettierrc,scss,md}':
+    'prettier --write --config .prettierrc .',
 
   '*.{js,ts,tsx}': 'eslint .',
 }
