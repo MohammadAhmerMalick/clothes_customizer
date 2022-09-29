9c8bda24f1df0bef42f04eb4c66b918e614059c4
unknown <mohammadahmermalick@gmail.com>
2022-09-30 01:00:36 +0500

    update: bordered, disbaled and title options added in Button Component

--- components/common/buttons/Button.tsx
+++ components/common/buttons/Button.tsx
@@ -5,22 +5,31 @@ import S from './Button.module.scss'
 import { ButtonInterface } from '../../../ts/interface'
 
 const Button: FC<ButtonInterface> = ({
+  title,
   children,
   onClick,
   className,
   primary,
+  disabled,
+  bordered,
 }) => {
   const handleOnclick = () => {
     if (!onClick) return
 
     onClick()
   }
-
   return (
     <button
+      title={title}
       type="button"
       onClick={handleOnclick}
-      className={classNames(S.button, { [S.primary]: primary }, className)}
+      className={classNames(
+        S.button,
+        { [S.primary]: primary },
+        { [S.bordered]: bordered },
+        className
+      )}
+      disabled={disabled}
     >
       {children}
     </button>
